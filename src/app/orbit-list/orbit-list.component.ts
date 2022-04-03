305acdbd890c6e834394aa0155b88acb19a32f91
SamLP2000 <SamLP2000@outlook.com>
2022-04-02 22:01:26 -0500

    Completed.

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -1,30 +1,40 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Satellite } from '../satellite';
+import { Component, OnInit, Input } from "@angular/core";
+import { Satellite } from "../satellite";
 
 @Component({
-  selector: 'app-orbit-list',
-  templateUrl: './orbit-list.component.html',
-  styleUrls: ['./orbit-list.component.css']
+  selector: "app-orbit-list",
+  templateUrl: "./orbit-list.component.html",
+  styleUrls: ["./orbit-list.component.css"],
 })
-
 export class OrbitListComponent implements OnInit {
-
   @Input() satellites: Satellite[];
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  zebraOtherStripe = true;
+  color = false;
+
+  sort(column: string): void {
+    // array.sort modifies the array, sorting the items based on the given compare function
+    this.satellites.sort(function (a: Satellite, b: Satellite): number {
+      if (a[column] < b[column]) {
+        return -1;
+      } else if (a[column] > b[column]) {
+        return 1;
+      }
+      return 0;
+    });
   }
-
-	sort(column: string): void {
-		// array.sort modifies the array, sorting the items based on the given compare function
-		this.satellites.sort(function (a: Satellite, b: Satellite): number {
-			if (a[column] < b[column]) {
-				return -1;
-			} else if (a[column] > b[column]) {
-				return 1;
-			}
-			return 0;
-		});
-	}
-
+  zebra(): boolean {
+    if (!this.color) {
+      this.color = true;
+      this.zebraOtherStripe = false;
+      return this.color;
+    } else if (this.color) {
+      this.color = false;
+      this.zebraOtherStripe = true;
+      return this.color;
+    }
+  }
 }
